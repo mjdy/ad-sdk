83575b6d60442d6e8099b83254133c9cedc13497
CX <www.chenxi2008@gmail.com>
2021-02-02 11:16:48 +0800

    update sdk

--- app/src/main/java/com/mjdy/ad/sample/MainActivity.java
+++ app/src/main/java/com/mjdy/ad/sample/MainActivity.java
@@ -1,10 +1,9 @@
 package com.mjdy.ad.sample;
 
-import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
--- app/src/main/java/com/mjdy/ad/sample/MyApplication.java
+++ app/src/main/java/com/mjdy/ad/sample/MyApplication.java
@@ -12,15 +12,7 @@ public class MyApplication extends Application {
         // 初始化广告sdk，最好放在程序入口，比如application里
         MJAd.init(this, "1KnLA300");  // init ad sdk
 
-
-        // OAID 尽量赋值. 以下两种方式均可
-
-        // 方式一
 //        MJAd.init(this,"appId","oaid","channel");
-
-        // 方式二
-//        MJAd.setOaid("oaid");
-//        MJAd.setChannel("channel");
 
     }
 }
