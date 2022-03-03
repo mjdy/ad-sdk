5dd2f9d3c98d506730e3006d63efbe0f8d8e0a46
CX <www.chenxi2008@gmail.com>
2022-03-03 15:25:18 +0800

    2.2.3

--- app/src/main/java/com/mjdy/ad/sample/MainActivity.java
+++ app/src/main/java/com/mjdy/ad/sample/MainActivity.java
@@ -156,12 +156,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
